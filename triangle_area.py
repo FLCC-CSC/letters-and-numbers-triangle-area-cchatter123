3a9030b9ec0ec1443787bf1991a323c359aed83a
daveflcc <143441096+daveflcc@users.noreply.github.com>
2025-02-18 23:44:58 -0500

    Update triangle_area.py

--- triangle_area.py
+++ triangle_area.py
@@ -1,5 +1,5 @@
 # FILE NAME - triangle_area.py
-
+# DRG - Rerun for points 2025-02-18-2343
 # NAME: Oliver Doty
 # DATE: 2/18/2025
 # BRIEF DESCRIPTION:  Getting the base and area of a triangle then calculating the area
